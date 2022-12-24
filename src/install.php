f21fc995b910b4bcfde4a5ef9e2340be1019aa9a
mchljams <10671926+mchljams@users.noreply.github.com>
2022-12-24 13:50:51 -0600

    move index.php into project build

--- src/install.php
+++ src/install.php
@@ -15,6 +15,8 @@ class Install {
     public function execute()
     {
         $this->publishDockerCompose();
+        $this->publishPhpDocumentorConfig();
+        $this->publishPhpUnitConfig();
     }
 
     public function publishDockerCompose()
@@ -25,6 +27,26 @@ class Install {
 
         // write the docker-compose to the project root
         file_put_contents('./docker-compose.yml', $this->content);
+    }
+
+    public function publishPhpDocumentorConfig()
+    {
+        // get the contents of the docker-compose
+        $this->content = file_get_contents('./vendor/integrity/tivoli/stubs/phpdoc.xml');
+
+
+        // write the docker-compose to the project root
+        file_put_contents('./phpdoc.xml', $this->content);
+    }
+
+    public function publishPhpUnitConfig()
+    {
+        // get the contents of the docker-compose
+        $this->content = file_get_contents('./vendor/integrity/tivoli/stubs/phpunit.xml');
+
+
+        // write the docker-compose to the project root
+        file_put_contents('./phpunit.xml', $this->content);
     }
 
 }
